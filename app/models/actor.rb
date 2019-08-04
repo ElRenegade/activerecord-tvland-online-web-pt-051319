2301d0d641faeba21c05782eed88fdca69f86a26
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-04 19:56:40 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def self.full_name
-    self.where(['fisrt_name = ?, last_name = ?', self.first_name,self.last_name])
+    '#{self.first_name} #{self.last_name}'
   end
 
 end
