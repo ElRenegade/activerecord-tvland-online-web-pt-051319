cb1c7e11c58edd0b3733f59c633cd7b64e2d6d7e
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-04 20:11:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,10 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
+
+  def actors_list
+    self.actor.collect do |t|
+      t.name
+    end
+  end
 end
